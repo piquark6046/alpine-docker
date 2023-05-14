93ab18dee67d932270f26ba414e5bbe0dcc51a1f
PiQuark6046 <piquark6046@proton.me>
2023-05-14 14:15:53 +0000

    Revert chages

--- init_docker.sh
+++ init_docker.sh
@@ -1,6 +1,3 @@
 # Set location of Npm installed globally
 npm config set prefix /usr/local
-
-# Install wasm-pack
 curl --proto '=https' --tlsv1.3  https://sh.rustup.rs -sSf | sh -s -- -y
-cargo install wasm-pack
