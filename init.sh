97ba02fd858fcc8be9a243b4715c8a71c35b726d
PiQuark6046 <piquark6046@proton.me>
2023-05-21 12:05:48 +0000

    update npm

--- init.sh
+++ init.sh
@@ -20,6 +20,7 @@ sudo apt install gcc g++ make
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
+npm update -g npm
 
 # npm packages
 NpmPackages=("ts-node" "tslib" "typescript")
