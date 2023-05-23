4ca59e9ac345170ab1a69e9a0cd1ea5e3a841ae2
PiQuark6046 <piquark6046@proton.me>
2023-05-23 08:03:11 +0000

    Discard gitcache

--- init_docker.sh
+++ init_docker.sh
@@ -1,3 +1,7 @@
 # Set location of Npm installed globally
 npm config set prefix /usr/local
 curl --proto '=https' --tlsv1.3  https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Configure Git maintenance
+git config --global maintenance.strategy incremental
+git config --global pack.packSizeLimit 128m
