b187fdec6b8c691db577ca43d871c0e25dfae7c1
PiQuark6046 <piquark6046@proton.me>
2023-07-11 14:15:24 +0000

    Enforce TLS 1.3

--- init.sh
+++ init.sh
@@ -17,9 +17,9 @@ done
 yes | unminimize
 
 # NodeJS
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt install -y nodejs
+curl --tlsv1.3 -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt install -y nodejs
 sudo apt install gcc g++ make
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+curl --tlsv1.3 -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 npm update -g npm
